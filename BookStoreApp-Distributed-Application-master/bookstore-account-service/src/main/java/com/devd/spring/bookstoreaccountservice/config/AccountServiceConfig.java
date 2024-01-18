f8479be1ae9b44e45e252807ac6279f88fea3bc9
ankit <ankitagrawal15434@gmail.com>
2024-01-18 23:12:39 +0530

    Initial commit

---
+++ BookStoreApp-Distributed-Application-master/bookstore-account-service/src/main/java/com/devd/spring/bookstoreaccountservice/config/AccountServiceConfig.java
@@ -0,0 +1,11 @@
+package com.devd.spring.bookstoreaccountservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Ankit Agrawal, Date : 2019-07-14
+ */
+@Configuration
+public class AccountServiceConfig {
+
+}
